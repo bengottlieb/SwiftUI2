228dc8971557a8a57e0cdb6d072309f739f74fd0
Ben Gottlieb <ben@standalone.com>
2020-07-11 11:14:10 -0500

    Initial Commit

---
+++ Shared/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Shared
+//
+//  Created by Ben Gottlieb on 7/11/20.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
